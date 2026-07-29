b8d4b842b83737d475a00eb0321768edf47cd2d3
Minh Nguyen <nmnminh2005@gmail.com>
2026-07-29 16:53:54 -0700

    change structure

--- my_big_number.py
+++ my_big_number.py
@@ -1,22 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 20 12:29:29 2026
+import logging
 
-@author: Owner
-"""
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s: %(message)s"
+)
 
 
 class MyBigNumber:
 
     def sum(self, stn1, stn2):
+        logging.info("Starting addition: %s + %s", stn1, stn2)
+
         i = len(stn1) - 1
         j = len(stn2) - 1
-
         carry = 0
         result = []
 
-        while i >= 0 or j >= 0 or carry:
-
+        while i >= 0 or j >= 0 or carry > 0:
             digit1 = 0
             digit2 = 0
 
@@ -29,11 +30,19 @@ class MyBigNumber:
                 j -= 1
 
             total = digit1 + digit2 + carry
-
             result.append(str(total % 10))
-
             carry = total // 10
 
-        result.reverse()
+            logging.info(
+                "Step: %d + %d + carry = %d",
+                digit1,
+                digit2,
+                total
+            )
 
-        return "".join(result)
+        result.reverse()
+        answer = "".join(result)
+
+        logging.info("Result: %s", answer)
+
+        return answer
