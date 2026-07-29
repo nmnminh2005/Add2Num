8fec1d2312cf862bbb6efc1b814e855287165ad0
Minh Nguyen <nmnminh2005@gmail.com>
2026-07-29 16:53:30 -0700

    Change structure

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 20 12:26:18 2026
-
-@author: Owner
-"""
-
-
 from my_big_number import MyBigNumber
+
 
 calculator = MyBigNumber()
 
-answer = calculator.sum("1234", "897")
+result = calculator.sum("1234", "897")
 
-print(answer)
+print("Result:", result)
